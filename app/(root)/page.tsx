3368e94fa694f68363d4909985f6aeb164be18d3
AliRaza192 <alirazaqasim192@gmail.com>
2024-05-12 12:20:02 +0500

    name error

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,11 @@
 import HeaderBox from "@/components/HeaderBox";
 import RightSidebar from "@/components/RightSidebar";
 import TotalBalanceBox from "@/components/TotalBalanceBox";
+import { getLoggedInUser } from "@/lib/actions/user.actions";
 import React from "react";
 
-function Home() {
-  const loggedIn = { firstName: "Ali", lastName: "Raza", email: "alirazaqasim192@gmail.com" };
+ const Home = async () => {
+  const loggedIn = await getLoggedInUser();
   return (
     <section className="home">
       <div className="home-content">
@@ -12,7 +13,7 @@ function Home() {
           <HeaderBox
             type="greeting"
             title="Welcome"
-            user={loggedIn.firstName || "Guest"}
+            user={loggedIn?.name || "Guest"}
             subtext="Access and manage your account and transactions efficiently."
           />
           <TotalBalanceBox
@@ -21,7 +22,7 @@ function Home() {
             totalCurrentBalance={1250.35}
           />
         </header>
-       ReSEND TRANSCTIONS 
+       RESEND TRANSCTIONS 
       </div>
       <RightSidebar
         user={loggedIn}
